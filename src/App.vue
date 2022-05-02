dd83d7f313362844406fc951cfd78923feab2a34
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-02 08:51:41 -0400

    save

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <WeatherDash v-if="currentWeather" :currentWeather="currentWeather" />
+    <div v-else>
+      <h3>No Weather</h3>
+    </div>
+  </div>
+  
 </template>
 
 <script>
+import axios from 'axios'
+import WeatherDash from './components/WeatherDash.vue'
+const API_KEY = process.env.VUE_APP_WEATHER_KEY
+
   export default {
     name: 'App',
-    components: {}
+    components: {
+      WeatherDash,
+    },
+    data: () => ({
+      dailyWeather: [],
+      currentWeather: null
+    }),
+    mounted: function() {
+      navigator.geolocation.getCurrentPosition(async  position => {
+        await this.getCurrentWeather(position.coords)
+      })
+    },
+    methods: {
+      async getCurrentWeather(coords) {
+        const res = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords.latitude}&lon=${coords.longitude}&units=imperial&appid=${API_KEY}`)
+      this.currentWeather = res.data.current
+      }
+    }
   }
 </script>
 
